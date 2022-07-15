0d5dfe378d3b54c05c9f890fa4b6cd21d3527006
killer glass <killerglass231@gmail.com>
2022-07-15 11:00:45 -0300

    vamos la

--- react/gamagit/src/App.js
+++ react/gamagit/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(props) {
+  //atraves de props pode se passar tudo
+  const [usuario, setUsuario] = useState("");
+
+  function handlePesquisa(){
+    axios.get('https://api.github.com/users/${usuario}/repos').then(Response =>console.log(Response.data))
+   
+  }
+  return (//jsx
+ 
+    <>34er34
+      <input className='Usuario' placeholder='Usuario' value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <button type="button" onClick={handlePesquisa}> Pesquisar </button>
+    </>
+
   );
 }
 
